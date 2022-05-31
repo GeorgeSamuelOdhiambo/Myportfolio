e188268b6b6e8f60d5540c7dd8eaf056fb287fc5
Odhiambo George <georgeodhi98@gmail.com>
2022-05-31 21:38:19 +0300

    Url

--- src/components/Skills/Services.js
+++ src/components/Skills/Services.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Servicescad from "./Servicescad";
-const { REACT_APP_ROUTE_URLL } = process.env;
+const { REACT_APP_ROUTE_URL} = process.env;
 
 function Services() {
   const [servicesdata, setData] = useState();
@@ -8,9 +8,10 @@ function Services() {
 
   const textcolor = { color: "#e73131" };
   useEffect(() => {
-    fetch(`${REACT_APP_ROUTE_URLL}services`)
+    fetch(`${REACT_APP_ROUTE_URL}services`)
       .then((response) => response.json())
       .then((data) => {
+        console.log(data.info);
         setData(data.info);
         setDatadisplay(true);
       });
